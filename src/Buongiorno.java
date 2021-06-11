9302650dae70e4bf0234141c31a30d3cfa6bb355
Ackermann32 <a.califano123@gmail.com>
2021-06-11 13:17:14 +0200

    Prova Alfonso

--- src/Buongiorno.java
+++ src/Buongiorno.java
@@ -1,9 +1,12 @@
 
+
 public class Buongiorno {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello world");
+        System.out.println("Alfonso");
+        
 	}
 
 }
