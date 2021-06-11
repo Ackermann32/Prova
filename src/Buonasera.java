50b6089470c0df5912d7825bd14e224808cb20b1
Ackermann32 <a.califano123@gmail.com>
2021-06-11 15:38:32 +0200

    prova7

--- src/Buonasera.java
+++ src/Buonasera.java
@@ -3,7 +3,7 @@ public class Buonasera {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("michele risi");
+		System.out.println("michele");
 	}
 
 }
